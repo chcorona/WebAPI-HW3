c52cd6c3c7dad3250be0c1ab1f15726828380bf2
chcorona <christopher.corona@ucdenver.edu>
2017-03-11 22:12:17 -0700

    4 tests for HW3

---
+++ homework3/api/controllers/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"key1": {
+		"data": "0a7239c9a7004d3252fdbeb3f728ec894194bffea7ab0e3567a453c09057795f8c1b0bd4316aca4f72362a8bf3af2708"
+	}
+};
---
+++ homework3/api/controllers/store.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"sigad": {
+		"key": "key1",
+		"data": "2afaebdbfd5f830698bed8d0a59f8ea2495d8ec1f2a5323d58df0e6d6e13d65044ffcead7d16d8bfd7735900d685061aa77ebad21babbd29774442a0998b96d1"
+	}
+};
